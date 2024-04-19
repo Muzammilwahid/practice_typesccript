9b3ab3edc60bb0069d4d337b395df97fa459b61a
Muzammil wahid <160735299+Muzammilwahid@users.noreply.github.com>
2024-04-20 00:11:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+// let age = 18
+// let country = "Pakistan"
+// age =25
+// console.log(`my age is ${age}: and my country is ${country} `);
+// function fine() {
+//   console.log("hello world");
+// }
+// fine() //calling to function
+function greet(name, age, email) {
+    console.log(`
+    enter your name ${name} 
+    enter you age ${age}
+     and enter your ${email} `);
+}
+greet("muzammil", 19, "muzammilwahid0311020339@gmail.com");
+greet("haris", 20, "abcd@gmail.com");
+greet("fareed", 21, "XXXXXXXXXXXXX");
---
+++ main.ts
@@ -0,0 +1,26 @@
+// let age = 18
+// let country = "Pakistan"
+// age =25
+// console.log(`my age is ${age}: and my country is ${country} `);
+
+// function fine() {
+//   console.log("hello world");
+// }
+// fine() //calling to function
+
+
+function greet(name:string,age:number,email:string){
+    console.log(`
+    enter your name ${name} 
+    enter you age ${age}
+     and enter your ${email} `)
+}
+greet("muzammil",19,"muzammilwahid0311020339@gmail.com")
+greet("haris",20,"abcd@gmail.com")
+greet("fareed", 21, "XXXXXXXXXXXXX")
+
+
+
+
+
+
